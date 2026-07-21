c5952e60d1ddeee44cd6b223e5420e049be2ec8f
sh4dow6oy <109531621+sh4dow6oy@users.noreply.github.com>
2026-07-21 09:27:53 +0300

    Update vmpressure.c

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -262,8 +262,8 @@ static void vmpressure_work_fn(struct work_struct *work)
 	spin_unlock(&vmpr->sr_lock);
 
 	pressure = vmpressure_calc_pressure(scanned, reclaimed);
-	vmpr->pressure = pressure;
 	level = vmpressure_level(pressure);
+	vmpr->pressure = pressure;
 
 	do {
 		if (vmpressure_event(vmpr, level, ancestor, signalled))
